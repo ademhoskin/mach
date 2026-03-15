18cdf73d731c11fce88f7cc1ec3257bb24233004
Adem Hoskin <ademjhoskin@gmail.com>
2026-03-15 17:29:24 -0400

    refactor(node/wavetable: use std::variant instead of func ptr to speed up render_frame

--- core/nodes/wavetable/wavetable_osc.hpp
+++ core/nodes/wavetable/wavetable_osc.hpp
@@ -1,37 +1,79 @@
 #pragma once
 
 #include "core/common/constants.hpp"
+#include "core/nodes/node.hpp"
+#include "core/nodes/wavetable/wavetable.hpp"
 
 #include <cstdint>
 #include <span>
+#include <variant>
 
 namespace mach::nodes::wavetable {
 
 enum class Waveform : uint8_t { SINE, SAWTOOTH, TRIANGLE, SQUARE };
 
+using AnyWavetable = std::variant<Sine, Sawtooth, Triangle, Square>;
+
 class WavetableOscillator {
   public:
     explicit WavetableOscillator(float sample_rate = mach::constants::DEFAULT_SAMPLE_RATE,
-                                 Waveform waveform = Waveform::SINE) noexcept;
+                                 Waveform waveform = Waveform::SINE) noexcept
+        : sample_rate_ {sample_rate}, phase_increment_ {compute_phase_increment()},
+          wavetable_ {make_wavetable(waveform)}, waveform_ {waveform} {}
 
-    void set_sample_rate(float sample_rate) noexcept;
-    void set_frequency(float frequency_in_hz) noexcept;
-    void set_waveform(Waveform waveform) noexcept;
-    void render_frame(std::span<float> output) noexcept;
+    void set_sample_rate(float sample_rate) noexcept {
+        sample_rate_ = sample_rate;
+        phase_increment_ = compute_phase_increment();
+    }
+
+    void set_frequency(float frequency_hz) noexcept {
+        frequency_hz_ = frequency_hz;
+        phase_increment_ = compute_phase_increment();
+    }
+
+    void set_waveform(Waveform waveform) noexcept {
+        waveform_ = waveform;
+        wavetable_ = make_wavetable(waveform);
+    }
+
+    void render_frame(std::span<float> output) noexcept {
+        std::visit(
+            [&](const auto& table) -> void {
+                for (auto& sample : output) {
+                    sample = std::decay_t<decltype(table)>::get_interpolated_sample(phase_);
+                    phase_ += phase_increment_;
+                }
+            },
+            wavetable_);
+    }
 
   private:
-    [[nodiscard]] auto compute_phase_increment() const noexcept -> uint32_t;
-    void bind_interpolated_sample_ptr() noexcept;
+    [[nodiscard]] auto compute_phase_increment() const noexcept -> uint32_t {
+        return static_cast<uint32_t>(frequency_hz_ / sample_rate_
+                                     * mach::constants::TWO_TO_POWER_OF_32);
+    }
+
+    [[nodiscard]] static auto make_wavetable(Waveform waveform) noexcept -> AnyWavetable {
+        switch (waveform) {
+            case Waveform::SINE:
+                return Sine {};
+            case Waveform::SAWTOOTH:
+                return Sawtooth {};
+            case Waveform::TRIANGLE:
+                return Triangle {};
+            case Waveform::SQUARE:
+                return Square {};
+        }
+    }
 
     float sample_rate_;
     float frequency_hz_ {mach::constants::NOTE_A4};
-
     uint32_t phase_ {};
     uint32_t phase_increment_ {};
-    Waveform waveform_;
-    // NOTE: We DO NOT use std::function since it sucks in audio (dynamic alloc, vcall, can't
-    // inline)
-    float (*get_interpolated_sample_ptr_)(uint32_t) noexcept;
+    AnyWavetable wavetable_;
+    Waveform waveform_ {};
 };
+
+static_assert(GeneratorNode<WavetableOscillator>);
 
 } // namespace mach::nodes::wavetable
